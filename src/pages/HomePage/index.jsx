2120ed2bd89e1b5346d5a6cc6a220f98e55b4e26
jk6722 <jk6722@hanyang.ac.kr>
2023-07-30 12:23:12 +0900

    modify: header styling remove position: absolute in link container

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,6 +3,15 @@ import styles from "./HomePage.module.scss";
 import { useSetRecoilState } from "recoil";
 import { SelectedRouterAtom } from "../../recoil/SelectedRouter";
 
+const textData = {
+  subtitle: "SO, YOU WANT TO TRAVEL TO",
+  title: "SPACE",
+  description: `Let’s face it; if you want to go to space, 
+    you might as well genuinely go to outer space and not hover 
+    kind of on the edge of it. Well sit back, and relax 
+    because we’ll give you a truly out of this world experience!`,
+};
+
 const HomePage = () => {
   const navigate = useNavigate();
   const setSelectedRouter = useSetRecoilState(SelectedRouterAtom);
@@ -16,18 +25,13 @@ const HomePage = () => {
       <div className={styles.body_container}>
         <div className={styles.text_container}>
           <div className={styles.heading_five}>
-            <span>SO, YOU WANT TO TRAVEL TO</span>
+            <span>{textData.subtitle}</span>
           </div>
           <div className={styles.heading_one}>
-            <span>SPACE</span>
+            <span>{textData.title}</span>
           </div>
           <div className={styles.description}>
-            <p>
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
+            <p>{textData.description}</p>
           </div>
         </div>
         <div className={styles.btn_container} onClick={handleClickBtn}>
